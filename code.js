e12c65e73f1d3de2801be757d7a2b510f26fd944
colinmarchbanks <marchbanks.colin@gmail.com>
2019-10-11 10:36:46 -0500

    5 unit test done

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
-// helloWorld function 
-function helloWorld() {
+// // helloWorld function
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+// function expression syntax (assigning an anonymous function to a variable)
+const helloWorld = function() {
     return "Hello, World!";
-}
+};
+
+const sayHello = function (x) {
+    return "Hello, " + x + "!";
+};
--- tests.js
+++ tests.js
@@ -13,3 +13,21 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello',function () {
+    it('should be a defined function',function () {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe('string');
+    });
+    it('should return the string "Hello, C-dog!" when input is C-dog', function () {
+        expect(sayHello("C-dog")).toBe("Hello, C-dog!");
+    });
+    it('should return the string "Hello, Pat!" when input is Pat', function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
+    });
+});
